c5951851b84672f0f5d2c5cebfdda8854aee6b22
Cameron Armstrong <Camarmstr@gmail.com>
2022-04-14 20:12:51 -0400

    More Exercises

---
+++ contracts/forwhile.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.10;
+
+
+contract Loop {
+    function loop() public {
+        for (uint i = 0; i<10; i++) {
+            if (i == 3) {
+                continue;
+            }
+    
+            if (i == 5) {
+                break;
+            }
+        }
+
+        uint j;
+        while (j < 10) {
+            j++;
+        }
+    }
+
+
+
+
+}
