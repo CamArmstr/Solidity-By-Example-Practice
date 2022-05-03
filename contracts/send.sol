c8346dfd4d6dfa230c935c724b8853309e56ffa1
Cameron Armstrong <52431864+CamArmstr@users.noreply.github.com>
2022-05-03 08:48:06 -0400

    Create send.sol

---
+++ contracts/send.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.10;
+
+contract ReceiveEther {
+    receive() external payable {}
+    fallback() external payable {}
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+contract SendEther {
+    function sendViaTransfer(address payable _to) public payable {
+        _to.transfer(msg.value);
+    }
+
+    function sendViaSend(address payable _to) public payable {
+        bool sent = _to.send(msg.value);
+        require(sent,"not sent");
+    }
+
+    function sendViaCall(address payable _to) public payable {
+        (bool sent, bytes memory data) = _to.call{value:msg.value}("");
+        require(sent, "not sent");
+    }
+}
